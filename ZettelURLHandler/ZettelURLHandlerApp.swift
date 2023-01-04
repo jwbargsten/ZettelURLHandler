c5294c58af439b183c97636a241decd3bfc56a5a
Joachim Bargsten <jw@bargsten.org>
2023-01-04 13:12:38 +0100

    Initial Commit

---
+++ ZettelURLHandler/ZettelURLHandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZettelURLHandlerApp.swift
+//  ZettelURLHandler
+//
+//  Created by Joachim Bargsten on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZettelURLHandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
